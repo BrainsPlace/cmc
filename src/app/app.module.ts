5edf7d0e6698b84506be685c4acf7c8f41c19fb7
Mcclaugherty, Brian <MCCLAB3@nationwide.com>
2019-01-04 12:27:25 -0500

    counter animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountComponent } from './count/count.component';
 import { HttpClientModule} from '@angular/common/http';
+import { CountUpModule } from 'countup.js-angular2';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    CountUpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
