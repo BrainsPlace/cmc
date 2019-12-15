a489a27200a5e60623f2faa46c689ee5233df67e
Brian Mcclaugherty <brianmcclaugherty@gmail.com>
2019-12-15 18:25:44 -0500

    switch data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,13 +8,13 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getOverview() {
-    //return this.http.get('https://my-json-server.typicode.com/brainsplace/demo/overview')
-    return this.http.get('http://localhost:3000/overview')
+    return this.http.get('https://my-json-server.typicode.com/brainsplace/demo/overview')
+    //return this.http.get('http://localhost:3000/overview')
   }
 
   getEvents() {
-    //return this.http.get('https://my-json-server.typicode.com/brainsplace/demo/events')
-    return this.http.get('http://localhost:3000/events')
+    return this.http.get('https://my-json-server.typicode.com/brainsplace/demo/events')
+    //return this.http.get('http://localhost:3000/events')
   } 
 
   getCoordsFromAddress(address: string) {
